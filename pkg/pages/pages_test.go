b05d09124efdf3664fd5d8a454471c3091a9f634
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2018-12-23 01:27:39 +0300

    develop testing

---
+++ pkg/pages/pages_test.go
@@ -0,0 +1,57 @@
+package pages
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/isacikgoz/tldr/pkg/config"
+)
+
+func TestRead(t *testing.T) {
+	var tests = []struct {
+		input    []string
+		expected *Page
+	}{
+		{[]string{"git", "add"}, &Page{Name: "git add"}},
+		{[]string{"git"}, &Page{Name: "git"}},
+	}
+	for _, test := range tests {
+		if p, err := Read(test.input); p.Name != test.expected.Name && err != nil {
+			t.Errorf("Test Failed: {%s} inputted, {%s} expected, recieved: {%s}", strings.Join(test.input, ""), test.expected.Name, p.Name)
+		}
+	}
+}
+
+func TestQueryCommon(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected *Page
+	}{
+		{"curl", &Page{Name: "curl"}},
+		{"wget", &Page{Name: "wget"}},
+		{"man", &Page{Name: "man"}},
+	}
+	for _, test := range tests {
+		if p, err := queryCommon(test.input); p.Name != test.expected.Name && err != nil {
+			t.Errorf("Test Failed: {%s} inputted, {%s} expected, recieved: {%s}", test.input, test.expected.Name, p.Name)
+		}
+	}
+}
+
+func TestQueryOS(t *testing.T) {
+	var tests = []struct {
+		osname   string
+		input    string
+		expected *Page
+	}{
+		{"osx", "brew", &Page{Name: "brew"}},
+		{"linux", "apt", &Page{Name: "apt"}},
+	}
+	for _, test := range tests {
+		if config.OSName() == test.osname {
+			if p, err := queryOS(test.input); p.Name != test.expected.Name && err != nil {
+				t.Errorf("Test Failed: {%s} inputted, {%s} expected, recieved: {%s}", test.input, test.expected.Name, p.Name)
+			}
+		}
+	}
+}
