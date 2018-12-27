ed73f257caed7e4af6d3be2480c34e7b89da9c36
İbrahim Serdar Açıkgöz <serdaracikgoz86@gmail.com>
2018-12-27 20:40:02 +0300

    new style to the page

--- js/main.js
+++ js/main.js
@@ -13,6 +13,14 @@ if (!!$.prototype.justifiedGallery) {
 $(document).ready(function() {
 
   /**
+   * Shows the responsive navigation menu on mobile.
+   */
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
+
+
+  /**
    * Controls the different versions of  the menu in blog post articles 
    * for Desktop, tablet and mobile.
    */
