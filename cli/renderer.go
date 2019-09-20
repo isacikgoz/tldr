0b88256da77be8f54118db88b263b6aa5766ece0
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-09-21 00:52:18 +0300

    render tips commands and args

--- cli/renderer.go
+++ cli/renderer.go
@@ -1,6 +1,9 @@
 package cli
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/fatih/color"
 	"github.com/isacikgoz/gitin/term"
 	"github.com/isacikgoz/tldr/pages"
@@ -15,10 +18,26 @@ func renderItem(item interface{}, matches []int, selected bool) [][]term.Cell {
 	if selected {
 		line1 = append(line1, term.Cprint("> ", color.FgCyan)...)
 	} else {
-		line1 = append(line1, term.Cprint("  ", color.FgWhite)...)
+		line1 = append(line1, term.Cprint("  ")...)
 	}
 	line1 = append(line1, term.Cprint(tip.String(), color.FgHiBlue)...)
-	line2 := term.Cprint("  "+tip.Cmd.String(), color.FgHiCyan)
+
+	line2 := term.Cprint("  ", color.FgWhite)
+
+	s := fmt.Sprint(tip.Cmd)
+	start := 0
+	index := ""
+	if len(tip.Cmd.Args) == 0 {
+		line2 = append(line2, term.Cprint(s)...)
+	}
+	for _, arg := range tip.Cmd.Args {
+		s = strings.Replace(s, "{{"+arg+"}}", arg, 1)            // fix the arg
+		ts := s[start:strings.Index(s, arg)]                     // w/o arg
+		line2 = append(line2, term.Cprint(ts)...)                // append temp
+		line2 = append(line2, term.Cprint(arg, color.FgCyan)...) // append arg
+		index += ts + arg                                        // to keep the index of where to cut
+		start = len(index)
+	}
 
 	tipText = append(tipText, line1)
 	tipText = append(tipText, line2)
--- pages/tip.go
+++ pages/tip.go
@@ -10,9 +10,3 @@ func (t *Tip) String() string {
 	s := t.Desc
 	return s
 }
-
-// Display returns colored and indented text for rendering output
-func (t *Tip) Display() string {
-	s := " " + blue.Sprint(t.Desc) + "\n" + t.Cmd.Display()
-	return s
-}
