8b21599690184ecee91d31b1d558180d7b2b6a9a
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-09-21 00:56:56 +0300

    add some doc on the renderer

--- cli/renderer.go
+++ cli/renderer.go
@@ -11,9 +11,8 @@ import (
 
 func renderItem(item interface{}, matches []int, selected bool) [][]term.Cell {
 	var tipText [][]term.Cell
-
 	tip := item.(*pages.Tip)
-
+	// start rendering tip text
 	var line1 []term.Cell
 	if selected {
 		line1 = append(line1, term.Cprint("> ", color.FgCyan)...)
@@ -21,24 +20,22 @@ func renderItem(item interface{}, matches []int, selected bool) [][]term.Cell {
 		line1 = append(line1, term.Cprint("  ")...)
 	}
 	line1 = append(line1, term.Cprint(tip.String(), color.FgHiBlue)...)
-
+	// start rendering command template
 	line2 := term.Cprint("  ", color.FgWhite)
-
 	s := fmt.Sprint(tip.Cmd)
 	start := 0
 	index := ""
 	if len(tip.Cmd.Args) == 0 {
-		line2 = append(line2, term.Cprint(s)...)
+		line2 = append(line2, term.Cprint(s)...) // in case there is no args
 	}
 	for _, arg := range tip.Cmd.Args {
 		s = strings.Replace(s, "{{"+arg+"}}", arg, 1)            // fix the arg
-		ts := s[start:strings.Index(s, arg)]                     // w/o arg
-		line2 = append(line2, term.Cprint(ts)...)                // append temp
+		cmd := s[start:strings.Index(s, arg)]                    // w/o arg
+		line2 = append(line2, term.Cprint(cmd)...)               // append cmd
 		line2 = append(line2, term.Cprint(arg, color.FgCyan)...) // append arg
-		index += ts + arg                                        // to keep the index of where to cut
+		index += cmd + arg                                       // to keep the index of where to start next
 		start = len(index)
 	}
-
 	tipText = append(tipText, line1)
 	tipText = append(tipText, line2)
 	return tipText
